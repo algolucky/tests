5b0e10735adf19f5b993f280ae91abc6fb6935ff
Lucky Baar <lucky.baar@algorand.com>
2022-05-25 09:06:17 -0500

    go-algorand-2581: add separate Dockerfiles for verify test

--- algorand/go-algorand-2581/test.sh
+++ algorand/go-algorand-2581/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build . -f Dockerfile -t go-algorand:control --no-cache --progress plain
+docker build -f Dockerfile -t go-algorand:control --no-cache --progress plain .
 
-docker build . -f Dockerfile.in-path -t go-algorand:updater-in-path --no-cache --progress plain
+docker build -f Dockerfile.in-path -t go-algorand:updater-in-path --no-cache --progress plain .
+
+docker build -f Dockerfile.in-path-verify -t go-algorand:updater-in-path-verify --no-cache --progress plain .
