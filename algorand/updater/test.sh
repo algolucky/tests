a80d170a357b5f48d4b8b27dd1d9fcb379ec3ffb
Lucky Baar <lucky.baar@algorand.com>
2022-06-24 08:57:46 -0500

    update updater tests for algorand/go-algorand#4099

--- algorand/updater/test.sh
+++ algorand/updater/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 docker build -f Dockerfile -t go-algorand:control --no-cache --progress plain .
 
 docker build -f Dockerfile.in-path -t go-algorand:updater-in-path --no-cache --progress plain .
