8c1d9f21e84ef1b9dbcf2d3d2a2eea8ccc553568
Lucky Baar <lucky.baar@algorand.com>
2022-07-06 15:46:17 -0500

    go-algorand(updater): use newer update.sh

--- go-algorand/updater/test.sh
+++ go-algorand/updater/test.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-docker build -f Dockerfile -t go-algorand:control --no-cache --progress plain .
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-docker build -f Dockerfile.in-path -t go-algorand:updater-in-path --no-cache --progress plain .
+docker build -f "$SCRIPT_DIR"/Dockerfile -t go-algorand:control --no-cache --progress plain .
 
-docker build -f Dockerfile.in-path-verify -t go-algorand:updater-in-path-verify --no-cache --progress plain .
+docker build -f "$SCRIPT_DIR"/Dockerfile.in-path -t go-algorand:updater-in-path --no-cache --progress plain .
+
+docker build -f "$SCRIPT_DIR"/Dockerfile.in-path-verify -t go-algorand:updater-in-path-verify --no-cache --progress plain .
